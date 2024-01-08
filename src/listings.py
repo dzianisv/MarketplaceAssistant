49fba6eab7b597d22ed72ed8de6f1b502e10f1f0
Dennis <2119348+dzianisv@users.noreply.github.com>
2024-01-08 23:40:49 +0000

    Log a scrolling iteration

--- src/listings.py
+++ src/listings.py
@@ -9,52 +9,56 @@ import config
 
 logger = config.get_logger(__name__)
 
+
+def endOfListings(browser):
+    try:
+        browser.find_element(By.XPATH, "//*[contains(text(), 'Results from outside your search')]")
+        return True
+    except selenium.common.exceptions.NoSuchElementException:
+        return False
+
 def find_listings(browser, price_limit, city):
     # base_url = "https://www.facebook.com/marketplace"
     # url = f"{base_url}/{city}/search?query=rentals&exact=false"
 
     url = "https://www.facebook.com/marketplace/category/propertyrentals"
     browser.get(url)
-    sleep(1)
 
     # Regex pattern to identify price-like strings
     price_pattern = r"\$\d{1,3}(,\d{3})*(\.\d{2})?"
-    affordable_listings = []
+    affordable_listings = set()
     recent_len = -1
 
-    while len(affordable_listings) > recent_len:
-        recent_len = len(affordable_listings)
-        # Find all <a> elements containing a <span> that looks like a price 
-        a_elements = browser.find_elements(By.XPATH, "//a[.//span]")
-
-        for a_element in a_elements:
-            try:
-                span_element = a_element.find_element(By.TAG_NAME, "span")
-                price_text = span_element.text
-                listing_link = a_element.get_attribute("href")
-
-                if re.search(price_pattern, price_text):
-                    # price could contain a discount price, in this case it will look like: '$550\n$600'
-                    price_str = price_text.replace("$", "").replace(",", "").split('\n')[0]
-
-                    if price_str == 'Free':
-                        price = 0.0
-                    else:
-                        price = float(price_str)
-
-                    if price <= price_limit:
-                        affordable_listings.append(listing_link)
-            except Exception as e:
-                logger.error(f"Error processing a listing `{listing_link}: {e}")
-            
-        try:
-            browser.find_element(By.XPATH, "//*[contains(text(), 'Results from outside your search')]")
+    for i in range(100):
+        if endOfListings(browser):
             break
-        except selenium.common.exceptions.NoSuchElementException:
-            pass
-        
-        logger.debug("Scrolling to the bottom. Waiting for 3s...")
+        logger.debug("Scrolling to bottom of page, iteration %d", i)
         utils.scrollToBottom(browser)
         utils.wait(browser, 3)
+        
+    # Find all <a> elements containing a <span> that looks like a price 
+    a_elements = browser.find_elements(By.XPATH, "//a[.//span]")
 
-    return affordable_listings
+    for a_element in a_elements:
+        try:
+            span_element = a_element.find_element(By.TAG_NAME, "span")
+            price_text = span_element.text
+            listing_link = a_element.get_attribute("href")
+
+            if re.search(price_pattern, price_text):
+                # price could contain a discount price, in this case it will look like: '$550\n$600'
+                price_str = price_text.replace("$", "").replace(",", "").split('\n')[0]
+
+                if price_str == 'Free':
+                    price = 0.0
+                else:
+                    price = float(price_str)
+
+                if price <= price_limit:
+                    affordable_listings.add(listing_link)
+                    logger.debug("Added listing %f %s", price, listing_link)
+
+        except Exception as e:
+            logger.error(f"Error processing a listing `{listing_link}: {e}")
+        
+    return list(affordable_listings)
